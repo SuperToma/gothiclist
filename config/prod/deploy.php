d4a695b498440c898ca05f5fb4714c942bd58675
SuperToma <Thomas.HAMOU@supinfo.com>
2018-06-05 22:08:12 +0200

    Fix shared files

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -11,7 +11,7 @@ return new class extends DefaultDeployer
             ->deployDir('/home/gothiclist')
             ->repositoryUrl('git@github.com:SuperToma/gothiclist.git')
             ->repositoryBranch('master')
-            ->sharedFilesAndDirs(['.env', 'var/log'])
+            ->sharedFilesAndDirs(['var/log/'])
         ;
     }
 
