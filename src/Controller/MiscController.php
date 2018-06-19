9bb270424c79d7d9173cdb98b0603fb9953dc499
SuperToma <Thomas.HAMOU@supinfo.com>
2018-06-19 23:22:48 +0200

    WIP

--- src/Controller/MiscController.php
+++ src/Controller/MiscController.php
@@ -14,19 +14,44 @@ class MiscController extends Controller
      */
     public function contact(Request $request, \Swift_Mailer $mailer)
     {
-        $message = (new \Swift_Message('Contact form'))
-            ->setFrom('gothiclist@gmail.com')
-            ->setTo('gothiclist@gmail.com')
-            ->setBody(''.
-                '<b>Email : </b>' .$request->get('email').'<br />'.
-                '<b>Name : </b>' .$request->get('name').'<br />'.
-                '<b>Firstname : </b>' .$request->get('firstname').'<br />'.
-                '<b>Text : </b>' .$request->get('text').'<br />'
-            );
+        if($request->getMethod() === 'POST') {
+            if (filter_var($request->get('email'), FILTER_VALIDATE_EMAIL) === false) {
+                $this->addFlash('danger', 'Your email is not valid : <strong>' . $request->get('email') . '</strong>');
 
-        $mailer->send($message);
+                return $this->render('pages/misc/contact.html.twig', []);
+            }
 
-        $this->addFlash('success', 'Thank you for your message, we will try to reply you soon');
+            if (mb_strlen($request->get('text')) < 10) {
+                $this->addFlash('danger', 'Please provide a valid message');
+
+                return $this->render('pages/misc/contact.html.twig', []);
+            }
+
+            if (empty($request->get('name')) || empty($request->get('firstname'))) {
+                $this->addFlash('danger', 'Please provide your name & firstname');
+
+                return $this->render('pages/misc/contact.html.twig', []);
+            }
+
+            $message = (new \Swift_Message('Contact form'))
+                ->setFrom('gothiclist@gmail.com')
+                ->setTo('gothiclist@gmail.com')
+                ->setBody('<html><body>' .
+                    'Email : ' . $request->get('email') . "\n" .
+                    'Name : ' . $request->get('name') . "\n" .
+                    '<b>Firstname : </b>' . $request->get('firstname') . '<br />' .
+                    '<b>Text : </b>' . $request->get('text') . '<br />
+                    </body></html>'
+                );
+
+            $resp = $mailer->send($message);
+
+            if($resp === 1) {
+                $this->addFlash('success', 'Thank you for your message, we will try to reply you soon');
+            } else {
+                $this->addFlash('danger', 'Sorry, an error occurred while sending the email');
+            }
+        }
 
         return $this->render('pages/misc/contact.html.twig', []);
 
