ef55074b00a2bd5fe8fd2d94f8a72feed49b00b2
SuperToma <Thomas.HAMOU@supinfo.com>
2018-06-19 08:56:31 +0200

    Dev contact

--- src/Controller/MiscController.php
+++ src/Controller/MiscController.php
@@ -3,14 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class MiscController extends Controller
 {
     /**
-     *
+     * @param Request $request
+     * @param \Swift_Mailer $mailer
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function contact()
+    public function contact(Request $request, \Swift_Mailer $mailer)
     {
+        $message = (new \Swift_Message('Contact form'))
+            ->setFrom('gothiclist@gmail.com')
+            ->setTo('gothiclist@gmail.com')
+            ->setBody(''.
+                '<b>Email : </b>' .$request->get('email').'<br />'.
+                '<b>Name : </b>' .$request->get('name').'<br />'.
+                '<b>Firstname : </b>' .$request->get('firstname').'<br />'.
+                '<b>Text : </b>' .$request->get('text').'<br />'
+            );
+
+        $mailer->send($message);
+
+        $this->addFlash('success', 'Thank you for your message, we will try to reply you soon');
+
         return $this->render('pages/misc/contact.html.twig', []);
 
     }
