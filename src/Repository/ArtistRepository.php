df87b2014b61e37b1b2c7037adcf79374b2b313f
SuperToma <Thomas.HAMOU@supinfo.com>
2019-01-25 10:06:56 +0100

    WIP

--- src/Repository/ArtistRepository.php
+++ src/Repository/ArtistRepository.php
@@ -28,6 +28,32 @@ class ArtistRepository extends ServiceEntityRepository
     {
         $artists = $this->findBy([], ['name' => 'ASC']);
 
+        $sql = "
+        SELECT song.artist_id, style.name, COUNT(style.id) AS nbInStyle
+        FROM song
+        INNER JOIN `release` ON song.release_id = release.id
+        INNER JOIN release_style ON `release`.id = release_style.release_id
+        INNER JOIN style ON release_style.style_id = style.id
+        WHERE song.validated = 1
+        GROUP BY style.id
+        ORDER BY nbInStyle DESC;
+        ";
+/*
+        $qbd = $this->createQueryBuilder('song');
+        $qbd->addSelect('song.artist')
+            //->addSelect('style.name')
+            //->addSelect('COUNT(style.id) as nbInStyle')
+            ->from('App\Entity\Song', 'song')
+            ->innerJoin('song.release', 'release');
+           // ->innerJoin('release.id', 'release_style')
+            //->innerJoin('release_style.style_id', 'style')
+            //->where('song.validated = :songId')->setParameter('songId', 1)
+            //->groupBy('song.id')
+            //->orderBy('COUNT(style.id)', 'DESC');
+
+        $results = $qbd->getQuery()->getResult();
+        echo '<pre>'; print_r($results); exit();
+        */
         return $this->cleanArtistsNames($artists);
     }
 
